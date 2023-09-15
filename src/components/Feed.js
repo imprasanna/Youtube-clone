8172d9a4af3ff01e22d178d55f58856fc8b998b0
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-09-15 07:11:53 +0545

    Videos id and others fetched from api

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,9 +6,12 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {
+      setVideos(data.items);
+    });
   }, [selectedCategory]);
 
   return (
@@ -39,7 +42,7 @@ const Feed = () => {
           {selectedCategory} <span style={{ color: "#fc1503" }}>videos</span>
         </Typography>
 
-        <Videos />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,7 +1,23 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
+// import ChannelCard from "./ChannelCard";
+import VideoCard from "./VideoCard";
 
-const Videos = () => {
-  return <div>Videos</div>;
+const Videos = ({ videos }) => {
+  // console.log(videos);
+  return (
+    <>
+      <Stack flexWrap="wrap" direction="row" justifyContent="start" gap={2}>
+        {videos.map((item, index) => {
+          return (
+            <Box key={index}>
+              {item.id.videoId && <VideoCard video={item} />}
+            </Box>
+          );
+        })}
+      </Stack>
+    </>
+  );
 };
 
 export default Videos;
